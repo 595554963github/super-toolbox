940f252104a03ba5ed3a97727a55fde68a367a42
595554963github <595554963@qq.com>
2025-05-10 16:06:09 +0800

    Update MainForm.cs

--- MainForm.cs
+++ MainForm.cs
@@ -1,18 +1,63 @@
-using supertoolbox.Extractor;
+// MainForm.cs
 using System;
-using System.IO;
-using System.Windows.Forms;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
 
 namespace supertoolbox.Extractor
 {
     public partial class MainForm : Form
     {
         private int totalFileCount = 0;
+        private Dictionary<string, TreeNode> formatNodes = new Dictionary<string, TreeNode>();
 
         public MainForm()
         {
             InitializeComponent();
+            InitializeTreeView();
+        }
+
+        private void InitializeTreeView()
+        {
+            // 创建根节点
+            TreeNode audioNode = treeView1.Nodes.Add("音频");
+            TreeNode imageNode = treeView1.Nodes.Add("图片");
+            TreeNode otherNode = treeView1.Nodes.Add("其他档案");
+
+            // 定义提取器类型及其分类
+            var extractorTypes = new Dictionary<string, TreeNode>
+    {
+        { "RIFF - wave系列", audioNode },
+        { "RIFF - Fmod - bank", audioNode },
+        { "RIFF - wmav2 - xwma", audioNode },
+        { "RIFX - BigEndian - wem", audioNode },
+        { "RIFF - cdxa - xa", audioNode },
+        { "CRI - adpcm_adx - adx", audioNode },
+        { "CRI - adpcm_adx - ahx", audioNode },
+        { "Fmod - fsb5", audioNode },
+        { "Xiph.Org - Ogg", audioNode },
+        { "CRI - HCA - hca", audioNode },
+
+        { "RIFF - Google - webp", imageNode },
+        { "JPEG/JPG", imageNode },
+        { "PNG", imageNode },
+
+        { "其他格式1", otherNode },
+        { "其他格式2", otherNode }
+    };
+            var sortedExtractorTypes = extractorTypes.Keys.OrderBy(name => name).ToList();
+
+            foreach (string formatName in sortedExtractorTypes)
+            {
+                TreeNode parentNode = extractorTypes[formatName];
+                TreeNode node = parentNode.Nodes.Add(formatName);
+                formatNodes[formatName] = node;
+            }
+
+            treeView1.ExpandAll();
         }
 
         private void btnSelectFolder_Click(object sender, EventArgs e)
@@ -22,6 +67,7 @@ namespace supertoolbox.Extractor
                 if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
                 {
                     txtFolderPath.Text = folderBrowserDialog.SelectedPath;
+                    AppendMessageToRichTextBox($"已选择文件夹: {folderBrowserDialog.SelectedPath}");
                 }
             }
         }
@@ -35,232 +81,115 @@ namespace supertoolbox.Extractor
                 return;
             }
 
-            int choice = -1;
-            if (radioWave.Checked)
+            TreeNode selectedNode = treeView1.SelectedNode;
+            if (selectedNode == null)
             {
-                choice = 1;
-            }
-            else if (radioBank.Checked)
-            {
-                choice = 2;
-            }
-            else if (radioWebp.Checked)
-            {
-                choice = 3;
-            }
-            else if (radioXwma.Checked)
-            {
-                choice = 4;
-            }
-            else if (radioWem.Checked)
-            {
-                choice = 5;
-            }
-            else if (radioXa.Checked)
-            {
-                choice = 6;
-            }
-            else if (radioAdx.Checked)
-            {
-                choice = 7;
-            }
-            else if (radioAhx.Checked)
-            {
-                choice = 8;
-            }
-            else if (radioFsb5.Checked)
-            {
-                choice = 9;
-            }
-            else if (radioOgg.Checked)
-            {
-                choice = 10;
-            }
-            else if (radioJpg.Checked)
-            {
-                choice = 11;
-            }
-            else if (radioPng.Checked)
-            {
-                choice = 12;
-            }
-            else if (radioHca.Checked)
-            {
-                choice = 13;
-            }
-            if (choice == -1)
-            {
-                AppendMessageToRichTextBox("无效的选择。");
+                AppendMessageToRichTextBox("请选择一个具体的文件格式。");
                 return;
             }
+            string formatName = selectedNode.Text;
 
             totalFileCount = 0;
             try
             {
-                switch (choice)
+                var extractor = CreateExtractor(formatName);
+                if (extractor == null)
                 {
-                    case 1:
-                        var waveExtractor = new WaveExtractor();
-                        waveExtractor.FilesExtracted += (senderObj, fileNames) =>
-                        {
-                            foreach (string fileName in fileNames)
-                            {
-                                AppendMessageToRichTextBox(fileName);
-                                totalFileCount++;
-                            }
-                        };
-                        waveExtractor.Extract(dirPath);
-                        break;
-                    case 2:
-                        var bankExtractor = new BankExtractor();
-                        bankExtractor.FilesExtracted += (senderObj, fileNames) =>
-                        {
-                            foreach (string fileName in fileNames)
-                            {
-                                AppendMessageToRichTextBox(fileName);
-                                totalFileCount++;
-                            }
-                        };
-                        bankExtractor.Extract(dirPath);
-                        break;
-                    case 3:
-                        var webpExtractor = new WebpExtractor();
-                        webpExtractor.FilesExtracted += (senderObj, fileNames) =>
-                        {
-                            foreach (string fileName in fileNames)
-                            {
-                                AppendMessageToRichTextBox(fileName);
-                                totalFileCount++;
-                            }
-                        };
-                        webpExtractor.Extract(dirPath);
-                        break;
-                    case 4:
-                        var xwmaExtractor = new XwmaExtractor();
-                        xwmaExtractor.FilesExtracted += (senderObj, fileNames) =>
-                        {
-                            foreach (string fileName in fileNames)
-                            {
-                                AppendMessageToRichTextBox(fileName);
-                                totalFileCount++;
-                            }
-                        };
-                        xwmaExtractor.Extract(dirPath);
-                        break;
-                    case 5:
-                        var rifxExtractor = new RifxExtractor();
-                        rifxExtractor.FilesExtracted += (senderObj, fileNames) =>
-                        {
-                            foreach (string fileName in fileNames)
-                            {
-                                AppendMessageToRichTextBox(fileName);
-                                totalFileCount++;
-                            }
-                        };
-                        rifxExtractor.Extract(dirPath);
-                        break;
-                    case 6:
-                        var cdxaExtractor = new CdxaExtractor();
-                        cdxaExtractor.FilesExtracted += (senderObj, fileNames) =>
-                        {
-                            foreach (string fileName in fileNames)
-                            {
-                                AppendMessageToRichTextBox(fileName);
-                                totalFileCount++;
-                            }
-                        };
-                        cdxaExtractor.Extract(dirPath);
-                        break;
-                    case 7:
-                        var adxExtractor = new AdxExtractor();
-                        adxExtractor.FilesExtracted += (senderObj, fileNames) =>
-                        {
-                            foreach (string fileName in fileNames)
-                            {
-                                AppendMessageToRichTextBox(fileName);
-                                totalFileCount++;
-                            }
-                        };
-                        adxExtractor.Extract(dirPath);
-                        break;
-                    case 8:
-                        var ahxExtractor = new AhxExtractor();
-                        ahxExtractor.FilesExtracted += (senderObj, fileNames) =>
-                        {
-                            foreach (string fileName in fileNames)
-                            {
-                                AppendMessageToRichTextBox(fileName);
-                                totalFileCount++;
-                            }
-                        };
-                        ahxExtractor.Extract(dirPath);
-                        break;
-                    case 9:
-                        var fsb5Extractor = new Fsb5Extractor();
-                        fsb5Extractor.FilesExtracted += (senderObj, fileNames) =>
-                        {
-                            foreach (string fileName in fileNames)
-                            {
-                                AppendMessageToRichTextBox(fileName);
-                                totalFileCount++;
-                            }
-                        };
-                        fsb5Extractor.Extract(dirPath);
-                        break;
-                    case 10:
-                        var oggExtractor = new OggExtractor();
-                        oggExtractor.FilesExtracted += (senderObj, fileNames) =>
-                        {
-                            foreach (string fileName in fileNames)
-                            {
-                                AppendMessageToRichTextBox(fileName);
-                                totalFileCount++;
-                            }
-                        };
-                        oggExtractor.Extract(dirPath);
-                        break;
-                    case 11:
-                        var jpgExtractor = new JpgExtractor(richTextBox1);
-                        jpgExtractor.FilesExtracted += (senderObj, fileNames) =>
-                        {
-                            foreach (string fileName in fileNames)
-                            {
-                                AppendMessageToRichTextBox(fileName);
-                                totalFileCount++;
-                            }
-                        };
-                        jpgExtractor.Extract(dirPath);
-                        break;
-                    case 12:
-                        var pngExtractor = new PngExtractor();
-                        pngExtractor.FilesExtracted += (senderObj, fileNames) =>
-                        {
-                            foreach (string fileName in fileNames)
-                            {
-                                AppendMessageToRichTextBox(fileName);
-                                totalFileCount++;
-                            }
-                        };
-                        pngExtractor.Extract(dirPath);
-                        break;
-                    case 13:
-                        var hcaExtractor = new HcaExtractor();
-                        hcaExtractor.FilesExtracted += (senderObj, fileNames) =>
-                        {
-                            foreach (string fileName in fileNames)
-                            {
-                                AppendMessageToRichTextBox(fileName);
-                                totalFileCount++;
-                            }
-                        };
-                        hcaExtractor.Extract(dirPath);
-                        break;
+                    AppendMessageToRichTextBox($"错误: 不支持的格式 {formatName}");
+                    return;
                 }
+
+                // 内联 ExtractorAdapter 功能
+                Type extractorType = extractor.GetType();
+                EventInfo? eventInfo = extractorType.GetEvent("FilesExtracted");
+
+                if (eventInfo == null)
+                {
+                    throw new InvalidOperationException($"提取器类型 {extractorType.Name} 不包含 FilesExtracted 事件");
+                }
+
+                // 确保 EventHandlerType 不为 null
+                Type eventHandlerType = eventInfo.EventHandlerType
+                    ?? throw new InvalidOperationException($"事件 {eventInfo.Name} 的处理程序类型未知");
+
+                MethodInfo? invokeMethod = eventHandlerType.GetMethod("Invoke");
+                if (invokeMethod == null || invokeMethod.GetParameters().Length != 2)
+                {
+                    throw new InvalidOperationException("事件处理程序格式不正确");
+                }
+
+                Type eventArgsType = invokeMethod.GetParameters()[1].ParameterType;
+
+                // 根据事件参数类型创建适当的处理程序
+                if (eventArgsType == typeof(List<string>))
+                {
+                    EventHandler<List<string>> handler = (s, fileNames) =>
+                    {
+                        foreach (string fileName in fileNames)
+                        {
+                            AppendMessageToRichTextBox(fileName);
+                            totalFileCount++;
+                        }
+                    };
+
+                    Delegate handlerDelegate = Delegate.CreateDelegate(
+                        eventHandlerType,
+                        handler.Target,
+                        handler.Method);
+
+                    eventInfo.AddEventHandler(extractor, handlerDelegate);
+                }
+                else if (eventArgsType == typeof(string[]))
+                {
+                    EventHandler<string[]> handler = (s, fileNames) =>
+                    {
+                        foreach (string fileName in fileNames)
+                        {
+                            AppendMessageToRichTextBox(fileName);
+                            totalFileCount++;
+                        }
+                    };
+
+                    Delegate handlerDelegate = Delegate.CreateDelegate(
+                        eventHandlerType,
+                        handler.Target,
+                        handler.Method);
+
+                    eventInfo.AddEventHandler(extractor, handlerDelegate);
+                }
+                else
+                {
+                    throw new NotSupportedException($"不支持的事件参数类型: {eventArgsType.Name}");
+                }
+
+                AppendMessageToRichTextBox($"开始提取 {formatName} 格式的文件...");
+                extractor.Extract(dirPath);
                 AppendMessageToRichTextBox($"提取操作完成，总共提取了 {totalFileCount} 个文件。");
             }
             catch (Exception ex)
             {
                 AppendMessageToRichTextBox($"提取过程中出现错误: {ex.Message}");
+            }
+        }
+
+        private BaseExtractor? CreateExtractor(string formatName)
+        {
+            switch (formatName)
+            {
+                case "RIFF - wave系列": return new WaveExtractor();
+                case "RIFF - Fmod - bank": return new BankExtractor();
+                case "RIFF - Google - webp": return new WebpExtractor();
+                case "RIFF - wmav2 - xwma": return new XwmaExtractor();
+                case "RIFX - BigEndian - wem": return new RifxExtractor();
+                case "RIFF - cdxa - xa": return new CdxaExtractor();
+                case "CRI - adpcm_adx - adx": return new AdxExtractor();
+                case "CRI - adpcm_adx - ahx": return new AhxExtractor();
+                case "Fmod - fsb5": return new Fsb5Extractor();
+                case "Xiph.Org - Ogg": return new OggExtractor();
+                case "JPEG/JPG": return new JpgExtractor();
+                case "PNG": return new PngExtractor();
+                case "CRI - HCA - hca": return new HcaExtractor();
+                default: return null;
             }
         }
 
@@ -271,7 +200,61 @@ namespace supertoolbox.Extractor
 
         private void AppendMessageToRichTextBox(string message)
         {
-            richTextBox1.AppendText($"{DateTime.Now:yyyy - MM - dd HH:mm:ss} - {message}{Environment.NewLine}");
+            richTextBox1.AppendText($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - {message}{Environment.NewLine}");
+            richTextBox1.ScrollToCaret();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node != null)
+            {
+                labelStatus.Text = $"已选择: {e.Node.Text}";
+            }
+        }
+
+        private void treeViewContextMenu_Opening(object sender, CancelEventArgs e)
+        {
+            // 根据节点类型启用/禁用菜单项
+            if (treeView1.SelectedNode == null || treeView1.SelectedNode.Parent == null)
+            {
+                e.Cancel = true; // 根节点不显示菜单
+                return;
+            }
+
+            // 可以在这里根据节点类型禁用某些菜单项
+        }
+
+        private void moveToAudioMenuItem_Click(object sender, EventArgs e)
+        {
+            MoveSelectedNodeToCategory("音频");
+        }
+
+        private void moveToImageMenuItem_Click(object sender, EventArgs e)
+        {
+            MoveSelectedNodeToCategory("图片");
+        }
+
+        private void moveToOtherMenuItem_Click(object sender, EventArgs e)
+        {
+            MoveSelectedNodeToCategory("其他档案");
+        }
+
+        private void MoveSelectedNodeToCategory(string category)
+        {
+            TreeNode selectedNode = treeView1.SelectedNode;
+            if (selectedNode == null || selectedNode.Parent == null)
+                return;
+
+            TreeNode? targetCategory = treeView1.Nodes.Cast<TreeNode>().FirstOrDefault(n => n.Text == category);
+            if (targetCategory == null || selectedNode.Parent == targetCategory)
+                return;
+
+            TreeNode oldParent = selectedNode.Parent;
+            targetCategory.Nodes.Add((TreeNode)selectedNode.Clone());
+            oldParent.Nodes.Remove(selectedNode);
+            treeView1.SelectedNode = targetCategory.LastNode;
+
+            AppendMessageToRichTextBox($"已将 {selectedNode.Text} 移动到 {category} 类别");
         }
     }
 }
